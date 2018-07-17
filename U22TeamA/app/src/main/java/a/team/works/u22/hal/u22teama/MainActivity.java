16725db14a4fb0ad1b4f9cbedf396ce57c6dc3b0
tsubatyama <tsubasagolf1@gmail.com>
2018-07-17 11:46:27 +0900

    #2 marge (#23)
    * coommit
    
    * Login+Registration
    
    * kotani change

--- U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/MainActivity.java
+++ U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/MainActivity.java
@@ -2,6 +2,13 @@ package a.team.works.u22.hal.u22teama;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.v7.app.AlertDialog;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_edit);
     }
+
+
+
 }
