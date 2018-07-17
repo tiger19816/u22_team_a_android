0882a800f840333acf3db941bed4be284667a316
Taku0009 <36213491+Taku0009@users.noreply.github.com>
2018-07-17 10:20:15 +0900

    Login+Registration

--- U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/NewRegistrationActivity.java
+++ U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/NewRegistrationActivity.java
@@ -1,16 +1,16 @@
 package a.team.works.u22.hal.u22teama;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.support.v7.app.AlertDialog;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class NewRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         final String TAG = "DialogTest";
-        ((Button)findViewById(R.id.Registration)).setOnClickListener(new OnClickListener(){
+        ((Button)findViewById(R.id.Registration)).setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
@@ -41,27 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 CharSequence input_password = text_Password.getText();
 
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogStyle);
-                new AlertDialog.Builder(MainActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(NewRegistrationActivity.this, R.style.AlertDialogStyle);
+                new AlertDialog.Builder(NewRegistrationActivity.this);
 
-                        builder.setTitle("確認");
-                        builder.setMessage("氏名："+ input_name + "\n"
-                                +"生年月日："+ input_birthday +  "\n"
-                                + "住所：" + input_address +  "\n"
-                                + "電話番号："+ input_phone_number + "\n"
-                                + "性別：" + input_sex + "\n"
-                                + "クレジット番号：" + input_credit_card_number + "\n"
-                                + "パスワード："+ input_password +"");
+                builder.setTitle("確認");
+                builder.setMessage("氏名："+ input_name + "\n"
+                        +"生年月日："+ input_birthday +  "\n"
+                        + "住所：" + input_address +  "\n"
+                        + "電話番号："+ input_phone_number + "\n"
+                        + "性別：" + input_sex + "\n"
+                        + "クレジット番号：" + input_credit_card_number + "\n"
+                        + "パスワード："+ input_password +"");
 
-              //      TextView textView = findViewById(android.R.id.message);
-              //      textView.setTextSize(48.0f);
+                //      TextView textView = findViewById(android.R.id.message);
+                //      textView.setTextSize(48.0f);
 
-                        builder.setPositiveButton("登録する", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                builder.setPositiveButton("登録する", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                                }
-                        })
+                    }
+                })
                         .setNegativeButton("訂正する", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
