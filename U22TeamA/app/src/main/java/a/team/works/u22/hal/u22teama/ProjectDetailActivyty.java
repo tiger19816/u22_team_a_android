9a0b3b11edf37550b908a746e0694591d14ac4d7
cmint1045 <buraxtukuxx@yahoo.co.jp>
2018-08-22 13:45:46 +0900

    imageView

--- U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/ProjectDetailActivyty.java
+++ U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/ProjectDetailActivyty.java
@@ -2,6 +2,8 @@ package a.team.works.u22.hal.u22teama;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,7 +42,7 @@ public class ProjectDetailActivyty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project_detail_activyty);
+        setContentView(R.layout.activity_project_detail_activity);
 
         //Intent intent = getIntent();
         //projectId = (intent.getStringExtra("projectId"));
@@ -136,17 +140,30 @@ public class ProjectDetailActivyty extends AppCompatActivity {
             Boolean isLogin = false;
             try {
                 JSONObject rootJSON = new JSONObject(result);
+                //画像
+                URL photo = new URL(rootJSON.getString("photo"));
+                InputStream is = photo.openStream();
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(bmp);
+                //日付
                 String postDate = rootJSON.getString("postDate");
                 TextView etPostDate = findViewById(R.id.tv_OrderDateInfo);
                 etPostDate.setText(postDate);
+                //場所
                 String place = rootJSON.getString("place");
                 TextView etPlace = findViewById(R.id.tv_PlaceInfo);
                 etPlace.setText(place);
+                //説明
                 String content = rootJSON.getString("content");
                 TextView etContent = findViewById(R.id.tv_ContentInfo);
                 etContent.setText(content);
             } catch (JSONException ex) {
                 Log.e(DEBUG_TAG, "JSON解析失敗", ex);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
