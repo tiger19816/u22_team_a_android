7bedcd45faf36e9f14af666c3ce4f175ea7c5cba
cmint1045 <buraxtukuxx@yahoo.co.jp>
2018-08-25 15:42:42 +0900

    寄付画面修正

--- U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/GetUrl.java
+++ U22TeamA/app/src/main/java/a/team/works/u22/hal/u22teama/GetUrl.java
@@ -5,7 +5,7 @@ package a.team.works.u22.hal.u22teama;
  fixedUrlの後にサーブレット名を追加し作成。
   */
 public class GetUrl {
-    private static final String fixedUrl = "http://59.106.223.97:8080/u22_team_a_web/";
+    private static final String fixedUrl = "http://192.168.42.242:8080/u22_team_a_web/";
 //    private static final String fixedUrl = "http://10.0.2.2:8080/u22_team_a_web/";    //ローカルホスト
 //private static final String fixedUrl = "http://192.168.1.101:8080/u22_team_a_web/";    //平井モバイルルータ
     public static final String LoginUrl = fixedUrl + "UserLoginServlet";
